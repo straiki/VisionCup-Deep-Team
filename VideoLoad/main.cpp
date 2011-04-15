f9d066dbe50e993b7293ccdd3fe7ecbbbc4d8964
Pavel Simurda <psimurda@gmail.com>
2011-04-15 21:26:20 +0200

    upload

--- VideoLoad/main.cpp
+++ VideoLoad/main.cpp
@@ -33,12 +33,13 @@ detect = new MYdetektor(NULL); // zpracuj frame
         IplImage *image = video->next_frame();
         //MYdisplay::ShowImage(image);
             double tt = (double)cvGetTickCount();
-            if(counter == 75){
-                        detect->FindFaces();
-                         MYdisplay::ShowImage(detect->MyFrame,'q');
-            }
-        detect->setFrame(image);
 
+//            if(counter == 75){
+//                        detect->FindFaces();
+//                         MYdisplay::ShowImage(detect->MyFrame,'q');
+//            }
+        detect->setFrame(image);
+detect->FindFaces();
             tt = (double)cvGetTickCount() - tt;
             cout << tt/(cvGetTickFrequency()*1000.) << "ms" << endl;
 
