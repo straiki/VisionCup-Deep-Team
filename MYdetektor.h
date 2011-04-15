00264dd1a0677b93739f9611ef6d78d7308818cd
Pavel Simurda <psimurda@gmail.com>
2011-04-15 14:14:22 +0200

    Pridany tridy

---
+++ MYdetektor.h
@@ -0,0 +1,36 @@
+#ifndef MYDETEKTOR_H
+#define MYDETEKTOR_H
+
+#include <cv.h>
+#include <highgui.h>
+
+#include <iostream>
+#include <vector>
+using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <math.h>
+#include <float.h>
+#include <limits.h>
+#include <time.h>
+#include <ctype.h>
+
+class MYdetektor
+{
+    public:
+        MYdetektor();
+        virtual ~MYdetektor();
+    protected:
+    private:
+        /** @var IplImage * MyFrame
+        *   @brief Obrazek daneho framu, ve kterem se hledaji obliceje
+        */
+        IplImage * MyFrame;
+        /** @var
+        *   @brief
+        */
+};
+
+#endif // MYDETEKTOR_H
---
+++ MYdisplay.h
@@ -0,0 +1,14 @@
+#ifndef MYDISPLAY_H
+#define MYDISPLAY_H
+
+
+class MYdisplay
+{
+    public:
+        MYdisplay();
+        virtual ~MYdisplay();
+    protected:
+    private:
+};
+
+#endif // MYDISPLAY_H
