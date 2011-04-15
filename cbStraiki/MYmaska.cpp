d18331711d023b3b848172db4ec96b90847f9b25
Tomas Vahalik <tomas@vahalik.cz>
2011-04-15 17:05:23 +0200

    Prubezne zmeny

--- cbStraiki/MYmaska.cpp
+++ cbStraiki/MYmaska.cpp
@@ -11,3 +11,12 @@ MYmaska::~MYmaska(){
 	cout << "znicena maska" << endl;
 }
 
+void MYmaska::changeSize(float size){
+   // IplImage *source = cvLoadImage("obrazek.png");
+
+    IplImage *destination = cvCreateImage(cvSize((int)(this->source->width*size),
+                                                 (int)(this->source->height*size)),
+                                          source->depth,
+                                          source->nChannels);
+    cvResize(source, destination);
+}
--- cbStraiki/MYmaska.h
+++ cbStraiki/MYmaska.h
@@ -1,18 +1,24 @@
 #ifndef MYMASKA_H
 #define MYMASKA_H
 
+#include "stdhead.h"
+
 class MYmaska
 {
 
 //promenne
 private:
-	
+
 
 //funkce
 public:
 	MYmaska();	//konstruktor
 	virtual ~MYmaska();	//destruktor
-	
+
+    IplImage *source;
+
+	void changeSize(float);
+
 };
 
 #endif // MYMASKA_H
