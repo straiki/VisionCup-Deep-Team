daa31f04757fed19ae7a40b80f979cfb1fed6eec
Pavel Simurda <psimurda@gmail.com>
2011-04-16 15:36:18 +0200

    modified:   VideoLoad/MYdetektor.cpp 	modified:   VideoLoad/main.cpp

--- VideoLoad/MYdetektor.cpp
+++ VideoLoad/MYdetektor.cpp
@@ -53,17 +53,17 @@ CvMemStorage* storage = 0;
            // cout << "Nasel facy" << endl;
             CvRect *face = (CvRect*)cvGetSeqElem(faces, i);
 
-            cvSetImageROI(this->MyFrame,cvRect(face->x,face->y, face->width,face->height/2.0));
+            cvSetImageROI(this->MyFrame,cvRect(face->x,face->y + (face->height/5.5), face->width,face->height/3.0));
             //cvSetImageROI(this->MyFrame,*face);
-            //MYdisplay::ShowImage(this->MyFrame,'x');
+            MYdisplay::ShowImage(this->MyFrame,'x');
             if(FindEyes() > 0){// hledam oci v ramci ksichtu
                 cvResetImageROI(this->MyFrame);
                 //cvRectangle( this->MyFrame, cvPoint(face->x,face->y), cvPoint(face->x+face->width,face->y+face->height), CV_RGB(255,25,55), 2, 8, 0 );
                 Xakt.rFace = *face;
                 Xakt.eye1.x += Xakt.rFace.x;
-                Xakt.eye1.y += Xakt.rFace.y;
+                Xakt.eye1.y += Xakt.rFace.y + (face->height/5.5);
                 Xakt.eye2.x += Xakt.rFace.x;
-                Xakt.eye2.y += Xakt.rFace.y;
+                Xakt.eye2.y += Xakt.rFace.y + (face->height/5.5);
                 sX.push_back(Xakt);
             }
             else{
--- VideoLoad/main.cpp
+++ VideoLoad/main.cpp
@@ -30,7 +30,7 @@ double tta = (double)cvGetTickCount();
         detect->setFrame(image);
 
         double tt = (double)cvGetTickCount();
-        if(counter >= 0){
+        if(counter >= 100){
 //            detect->FindFaces();
 //            detect->DrawSezOblic();
             detect->FindFaces();
